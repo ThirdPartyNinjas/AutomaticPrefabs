80a43136219fc85c487c36e48313f7bc362b9a01
Jesse Chounard <jesse@thirdpartyninjas.com>
2017-11-21 10:36:21 -0600

    Updated to the current PlaymodeStateChanged API changes from Unity. Example project updated to the current version of Unity. (2017.2.0f3)

--- Assets/Automatic Prefabs/Editor/AutomaticPrefabsEditor.cs
+++ Assets/Automatic Prefabs/Editor/AutomaticPrefabsEditor.cs
@@ -7,61 +7,61 @@ using UnityEngine;
 
 namespace AutomaticPrefabs
 {
-	[InitializeOnLoad]
-	public class AutomaticPrefabsEditor
-	{
-		static AutomaticPrefabsEditor()
-		{
-			EditorApplication.playmodeStateChanged += PlaymodeStateChanged;
+    [InitializeOnLoad]
+    public class AutomaticPrefabsEditor
+    {
+        static AutomaticPrefabsEditor()
+        {
+            EditorApplication.playModeStateChanged += PlaymodeStateChanged;
 
-			DirectoryInfo directory = new DirectoryInfo(Application.dataPath + "/" + Automator.ResourcePath);
-			if (!directory.Exists)
-				directory.Create();
+            DirectoryInfo directory = new DirectoryInfo(Application.dataPath + "/" + Automator.ResourcePath);
+            if (!directory.Exists)
+                directory.Create();
 
-			// Loop through all the types available, if any of them implement IAutomatic, create a GameObject, then make a prefab of it.
-			foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
-			{
-				foreach (Type type in assembly.GetTypes())
-				{
-					if (typeof(IAutomatic).IsAssignableFrom(type) && !type.IsAbstract)
-					{
-						GameObject gameObject = Resources.Load(Automator.PrefabFolder + type.Name) as GameObject;
-						if (gameObject != null)
-							continue;
+            // Loop through all the types available, if any of them implement IAutomatic, create a GameObject, then make a prefab of it.
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (Type type in assembly.GetTypes())
+                {
+                    if (typeof(IAutomatic).IsAssignableFrom(type) && !type.IsAbstract)
+                    {
+                        GameObject gameObject = Resources.Load(Automator.PrefabFolder + type.Name) as GameObject;
+                        if (gameObject != null)
+                            continue;
 
-						gameObject = new GameObject(type.Name);
-						gameObject.AddComponent(type);
+                        gameObject = new GameObject(type.Name);
+                        gameObject.AddComponent(type);
 
-						PrefabUtility.CreatePrefab("Assets/" + Automator.ResourcePath + type.Name + ".prefab", gameObject.gameObject);
+                        PrefabUtility.CreatePrefab("Assets/" + Automator.ResourcePath + type.Name + ".prefab", gameObject.gameObject);
 
-						GameObject.DestroyImmediate(gameObject);
-					}
-				}
-			}
-		}
+                        GameObject.DestroyImmediate(gameObject);
+                    }
+                }
+            }
+        }
 
-		private static void PlaymodeStateChanged()
-		{
-			if (!EditorApplication.isPlaying && EditorApplication.isPlayingOrWillChangePlaymode)
-			{
-				// If we're in here, the user just hit play, create an Automator if one doesn't exist
-				Automator automator = GameObject.FindObjectOfType<Automator>();
-				if (automator == null)
-				{
-					GameObject go = new GameObject("Automator");
-					go.AddComponent<Automator>();
-					go.hideFlags = HideFlags.NotEditable;
-				}
-			}
-			else if (!EditorApplication.isPlaying && !EditorApplication.isPlayingOrWillChangePlaymode)
-			{
-				// If we're here, the user just hit stop, destroy the Automator if it exists
-				Automator automator = GameObject.FindObjectOfType<Automator>();
-				if (automator != null)
-				{
-					GameObject.DestroyImmediate(automator.gameObject);
-				}
-			}
-		}
-	}
+        private static void PlaymodeStateChanged(PlayModeStateChange playMode)
+        {
+            if (playMode == PlayModeStateChange.EnteredPlayMode)
+            {
+                // The user just hit play, create an Automator if one doesn't exist
+                Automator automator = GameObject.FindObjectOfType<Automator>();
+                if (automator == null)
+                {
+                    GameObject go = new GameObject("Automator");
+                    go.AddComponent<Automator>();
+                    go.hideFlags = HideFlags.NotEditable;
+                }
+            }
+            else if (playMode == PlayModeStateChange.ExitingPlayMode)
+            {
+                // The user just hit stop, destroy the Automator if it exists
+                Automator automator = GameObject.FindObjectOfType<Automator>();
+                if (automator != null)
+                {
+                    GameObject.DestroyImmediate(automator.gameObject);
+                }
+            }
+        }
+    }
 }
